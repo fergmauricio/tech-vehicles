e0269e7ba7f047ff8821c1d059f4bb45b3d56706
Ferg, Mauricio <Ferg, Mauricio>
2025-11-21 17:14:53 -0300

    feat(core): Configuração global prefix, validation pipe e swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,33 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix("api/v1");
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle("Vehicles API")
+    .setDescription("API para gerenciamento de veículos para o teste técnico")
+    .setVersion("1.0")
+    .addTag("vehicles")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/docs", app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
