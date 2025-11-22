f0b757114a5dbb709240c93842a54e5589f900a5
Ferg, Mauricio <Ferg, Mauricio>
2025-11-22 09:43:14 -0300

    chore(env): Adicionando env em App Module, conexão com Mongo em Database Module e env.example

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { DatabaseModule } from "./database/database.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/.env`,
+
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
